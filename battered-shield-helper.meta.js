cd684bf153958c4cc4f5bad32c153a77a0c6cde7
Art <a.molcanovas@gmail.com>
2017-02-07 20:36:15 +0000

    clicking an "AP full" notification should now open up the aura window
    Former-commit-id: 0ee8afa62b36a6c13bb981ed7908dd25312892a1

--- battered-shield-helper.meta.js
+++ battered-shield-helper.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Battered shield helper
 // @namespace    https://github.com/AlorelUserscripts/battered-shield-notifier
-// @version      0.2
+// @version      0.2.1
 // @downloadURL  https://raw.githubusercontent.com/AlorelUserscripts/battered-shield-notifier/master/battered-shield-helper.user.js
 // @updateURL    https://raw.githubusercontent.com/AlorelUserscripts/battered-shield-notifier/master/battered-shield-helper.meta.js
 // ==/UserScript==
--- battered-shield-helper.user.js
+++ battered-shield-helper.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Battered shield helper
 // @namespace    https://github.com/AlorelUserscripts/battered-shield-notifier
-// @version      0.2
+// @version      0.2.1
 // @description  Helps with your battered shields
 // @author       Alorel
 // @include      /^https?:\/\/(?=www\.)?batteredshield\.com\/game\/?/
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.1/knockout-min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/buzz/1.2.0/buzz.min.js
 // @icon64       https://cdn.rawgit.com/AlorelUserscripts/battered-shield-notifier/93f9519972e5cb25734c29fa241a90a150e55dbf/icon-64.png
@@ -18,16 +17,16 @@
 // @supportURL   https://github.com/AlorelUserscripts/battered-shield-notifier/issues
 // ==/UserScript==
 
-(function () {
-    var sfx = new buzz.sound("https://cdn.rawgit.com/AlorelUserscripts/battered-shield-notifier/a670d7c3fcf8d19a64bc8f1d151b0939ab72448a/notification.mp3", {
+document.addEventListener('DOMContentLoaded', function () {
+    const sfx = new buzz.sound("https://cdn.rawgit.com/AlorelUserscripts/battered-shield-notifier/a670d7c3fcf8d19a64bc8f1d151b0939ab72448a/notification.mp3", {
         preload: true,
         autoplay: false,
         loop: false
     });
 
-    function notify(msg, options) {
+    const notify = function (msg, options) {
         console.debug('Notifying: ' + msg);
-        options = $.extend(
+        options = Object.assign(
             {text: msg, timeout: 15000, sound: true},
             options || {}
         );
@@ -37,94 +36,95 @@
         if (options.sound) {
             sfx.play();
         }
-    }
+    };
 
-    var mutationObserverSettings = {childList: true, characterData: true, attributes: true, subtree: true};
-    var regex = {
+    const mutationObserverSettings = {childList: true, characterData: true, attributes: true, subtree: true};
+    const regex = {
         xOutOfY: /([0-9\.]+)\s*\/\s*([0-9\.]+)/
     };
 
-    function readyFn() {
-        document.removeEventListener('DOMContentLoaded', readyFn);
+    //Define model
+    const model = {
+        apPCT: ko.observable(0),
+        timer: ko.observable(0),
+        hpPCT: ko.observable(0),
+        captcha: ko.observable(false)
+    };
 
-        //Define model
-        var model = {
-            apPCT: ko.observable(0),
-            timer: ko.observable(0),
-            hpPCT: ko.observable(0),
-            captcha: ko.observable(false)
-        };
+    // Observe timer
+    (function () {
+        const node = document.querySelector(".timer_plaque");
+        new MutationObserver(function () {
+            const text = node.textContent.trim();
+            model.timer(isNaN(text) ? text : parseFloat(text));
+        }).observe(node, mutationObserverSettings);
+    })();
 
-        // Observe timer
-        (function () {
-            var node = document.querySelector(".timer_plaque");
-            new MutationObserver(function () {
-                var text = node.textContent.trim();
-                model.timer(isNaN(text) ? text : parseFloat(text));
-            }).observe(node, mutationObserverSettings);
-        })();
+    //Observe AP
+    (function () {
+        const node = document.querySelector(".attrib_value.ap_data");
+        new MutationObserver(function () {
+            const match = node.textContent.trim().match(regex.xOutOfY);
 
-        //Observe AP
-        (function () {
-            var node = document.querySelector(".attrib_value.ap_data");
-            new MutationObserver(function () {
-                var match = node.textContent.trim().match(regex.xOutOfY);
-
-                if (match) {
-                    model.apPCT(parseFloat(match[1]) / parseFloat(match[2]) * 100);
-                }
-            }).observe(node, mutationObserverSettings);
-        })();
-
-        //Observe HP
-        (function () {
-            var node = document.querySelector('.attrib_name[title="Hit Points"]').nextElementSibling;
-            new MutationObserver(function () {
-                var match = node.textContent.trim().match(regex.xOutOfY);
-
-                if (match) {
-                    model.hpPCT(parseFloat(match[1]) / parseFloat(match[2]) * 100);
-                }
-            }).observe(node, mutationObserverSettings);
-        })();
-
-        //Observe captcha
-        (function () {
-            var $node = $(".PopupCaptcha");
-            new MutationObserver(function () {
-                model.captcha($node.is(":visible"));
-            }).observe($node[0], mutationObserverSettings);
-        })();
-
-        // Subscribe
-        model.timer.subscribe(function (newVal) {
-            console.debug('Timer: ' + newVal);
-            // clearTimeout(timerTimeout);
-            if (newVal === 'Stopped') {
-                notify('Actions finished: ' + newVal);
+            if (match) {
+                model.apPCT(parseFloat(match[1]) / parseFloat(match[2]) * 100);
             }
-        });
+        }).observe(node, mutationObserverSettings);
+    })();
 
+    //Observe HP
+    (function () {
+        const node = document.querySelector('.attrib_name[title="Hit Points"]').nextElementSibling;
+        new MutationObserver(function () {
+            const match = node.textContent.trim().match(regex.xOutOfY);
+
+            if (match) {
+                model.hpPCT(parseFloat(match[1]) / parseFloat(match[2]) * 100);
+            }
+        }).observe(node, mutationObserverSettings);
+    })();
+
+    //Observe captcha
+    (function () {
+        const node = document.querySelector(".PopupCaptcha");
+        new MutationObserver(function () {
+            model.captcha(getComputedStyle(node).visibility === "visible");
+        }).observe(node, mutationObserverSettings);
+    })();
+
+    // Subscribe
+    model.timer.subscribe(function (newVal) {
+        console.debug('Timer: ' + newVal);
+        // clearTimeout(timerTimeout);
+        if (newVal === 'Stopped') {
+            notify('Actions finished: ' + newVal);
+        }
+    });
+
+    (function () {
+        const options = {
+            onclick: function () {
+                (document.querySelector(`#Left_menu a[href="javascript:PopupHandler.Popup('Cast');"] div[data-selector]`) || document.createElement('div')).click();
+            }
+        };
         model.apPCT.subscribe(function (newVal) {
             console.debug('AP: ' + newVal.toFixed(3));
             if (newVal >= 100) {
-                notify('AP full!');
+                notify('AP full!', options);
             }
         });
+    })();
 
-        model.hpPCT.subscribe(function (newVal) {
-            console.debug('HP: ' + newVal.toFixed(3));
-            if (newVal >= 100) {
-                notify('HP full!');
-            }
-        });
+    model.hpPCT.subscribe(function (newVal) {
+        console.debug('HP: ' + newVal.toFixed(3));
+        if (newVal >= 100) {
+            notify('HP full!');
+        }
+    });
 
-        model.captcha.subscribe(function (visible) {
-            if (visible) {
-                notify('Captcha!', {timeout: Number.MAX_VALUE});
-            }
-        });
-    }
-
-    document.addEventListener('DOMContentLoaded', readyFn);
-})();
+    model.captcha.subscribe(function (visible) {
+        if (visible) {
+            notify('Captcha!', {timeout: Number.MAX_VALUE});
+        }
+    });
+}, {once: true, passive: true});
